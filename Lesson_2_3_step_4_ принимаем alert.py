afdef2407389e526e9469251d0f3426fd60b3880
k.novosol <little.killer.93.11@gmail.com>
2021-05-24 11:36:09 +0300

    принимаем алерт готово

--- Lesson_2_3_step_4_ принимаем alert.py
+++ Lesson_2_3_step_4_ принимаем alert.py
@@ -19,6 +19,19 @@ try:
     button = browser.find_element_by_tag_name("button")
     button.click()
 
+    confirm = browser.switch_to.alert
+    confirm.accept()
+
+    xValue=browser.find_element_by_id("input_value")
+    x = xValue.text
+    y= calc(x)
+
+    field = browser.find_element_by_id("answer")
+    field.send_keys(y)
+
+    button = browser.find_element_by_tag_name("button")
+    button.click()
+
 finally:
     # ожидание чтобы визуально оценить результаты прохождения скрипта
     time.sleep(10)
