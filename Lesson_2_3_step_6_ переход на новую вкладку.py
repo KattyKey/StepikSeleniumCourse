fb0205fa69c08419bcc9a6feb559d46979768696
k.novosol <little.killer.93.11@gmail.com>
2021-05-25 14:03:58 +0300

    Переход на новую страницу

---
+++ Lesson_2_3_step_6_ переход на новую вкладку.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.support.ui import Select
+import math
+import os
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+
+
+try: 
+    link = "http://suninjuly.github.io/redirect_accept.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    button = browser.find_element_by_tag_name("button")
+    button.click()
+
+    new_window = browser.window_handles[1]
+    browser.switch_to.window(new_window)
+
+    xValue=browser.find_element_by_id("input_value")
+    x = xValue.text
+    y= calc(x)
+
+    field = browser.find_element_by_id("answer")
+    field.send_keys(y)
+
+    button = browser.find_element_by_tag_name("button")
+    button.click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
