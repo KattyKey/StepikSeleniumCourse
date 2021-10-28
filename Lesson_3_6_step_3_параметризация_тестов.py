1e341e7b91bd2d4fa1df71f7bb7888755869343f
k.novosol <little.killer.93.11@gmail.com>
2021-10-28 12:11:06 +0300

    Переход на новую страницу

--- Lesson_3_6_step_3_параметризация_тестов.py
+++ Lesson_3_6_step_3_параметризация_тестов.py
@@ -3,6 +3,11 @@ import time
 import math
 
 from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+
 
 @pytest.fixture(scope="function")
 def browser():
@@ -12,10 +17,30 @@ def browser():
     print("\nquit browser..")
     browser.quit()
 
-@pytest.mark.parametrize('language', ["ru", "en-gb"])
-def test_guest_should_see_login_link(browser, language):
-    link = f"http://selenium1py.pythonanywhere.com/{language}/"
-    browser.get(link)
-    browser.find_element_by_css_selector("#login_link")
+link_set =[
+"https://stepik.org/lesson/236895/step/1"  ,
+"https://stepik.org/lesson/236896/step/1",
+"https://stepik.org/lesson/236897/step/1",
+"https://stepik.org/lesson/236898/step/1",
+"https://stepik.org/lesson/236899/step/1",
+"https://stepik.org/lesson/236903/step/1",
+"https://stepik.org/lesson/236904/step/1",
+"https://stepik.org/lesson/236905/step/1"
+] 
+
+@pytest.mark.parametrize('link_data', link_set)
+def test_data_from_links(browser, link_data):
+    browser.get(link_data)
     answer = math.log(int(time.time()))
+    print(answer)
+    time.sleep(2)
+    text = browser.find_element_by_class_name("ember-text-area.ember-view.textarea.string-quiz__textarea").send_keys(
+        str(answer))
+    WebDriverWait(browser, 5).until(
+        EC.element_to_be_clickable((By.CLASS_NAME, "submit-submission"))
+    ).click()
+    result = WebDriverWait(browser, 20).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "smart-hints__hint")))
+    assert "Correct!"==str(result.text), "Wrong answer!"
+
 
